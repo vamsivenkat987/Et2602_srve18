bd47a19c037ac1c71044dec484ae7895d66450d0
vamsivenkat987 <vamsivenkat1998@gmail.com>
2020-05-14 19:21:33 +0200

    remove commit

--- search.php
+++ search.php
@@ -1,1 +1,22 @@
-<?php?>
+<?php
+  include("config.php");
+  $searchingmac = $_GET[mac];
+  $sql = <<<EOF
+              SELECT * where mac like "%$searchingmac%" by mac;
+EOF;
+
+	$final = $db->query($sql);
+	while($row = $final->fetchArray(SQLITE3_ASSOC) ) {
+      echo   $row['IPADDRESS']." | ".$row['VLAN']." | ".$row['PORT']." | "."$searchstr"."\n";
+   }
+
+   $db->close();
+
+
+
+
+
+
+
+
+?>
