2ed64e54044bdd3d34d4ffce9359c25777564db0
vamsivenkat987 <vamsivenkat1998@gmail.com>
2020-05-31 19:55:48 +0200

    adddevice file

--- list.php
+++
@@ -1,9 +0,0 @@
-<?php
-include(config.php);
-
-$Final = $db->query('SELECT * FROM manager');
-while($row = $Final->fetchArray(SQLITE3_ASSOC) ) {
-	echo  $row['ipaddress']. "|" .$row['vlan']. "|" .$row['portnumber']. "|" .$row['macaddress']. "\n";
-}
-$db->close();
-?>
