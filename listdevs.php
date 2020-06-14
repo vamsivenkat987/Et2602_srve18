2bc122f6229f0c9e6829d7d6e3c824d8ebff9792
mounika120 <mounikapolisetti97@gmail.com>
2020-06-14 13:25:11 +0200

    listdevs file

--- listdevs.php
+++ listdevs.php
@@ -9,7 +9,7 @@ while ($device_info = $output->fetchArray()) {
 	echo $device_info[0]. "|" .$device_info[1]. "|" .$device_info[2]. "|" .$device_info[3]. "|" .$device_info[4]. "|" .$device_info[5]. "|" .$device_info[6]."|";
 
     if (empty($device_info[0]) && empty($device_info[1]) && empty($device_info[2]) && empty($device_info[3]) && empty($device_info[4]) && empty($device_info[5]) &&  empty($device_info[6])){
-    	echo "there is no	 data";
+    	echo "there is no data";
 
     }
 
